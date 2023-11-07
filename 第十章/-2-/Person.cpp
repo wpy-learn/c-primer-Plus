e563d1090822a0a11b9d61d6dc58e8e05f20542e
wpy-learn <140713813+wpy-learn@users.noreply.github.com>
2023-11-07 20:39:16 +0800

    Create Person.cpp

---
+++ 第十章/-2-/Person.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+class Person
+{
+public:
+	Person();
+	Person(const std::string& ln, const char* fn = "Heyyou");
+	void Show() const;
+	void FormalShow() const;
+	~Person();
+
+private:
+	static const int LIMIT = 25;
+	std::string lname;
+	char fname[LIMIT];
+};
+
+int main()
+{
+	Person one;
+	Person two("Smythecraft");
+	Person three("Dimwiddy", "Sam");
+	one.Show();
+	std::cout << std::endl;
+	one.FormalShow();
+}
+
+Person::Person()
+{
+	lname = ""; fname[0] = '\0';
+}
+
+Person::Person(const std::string& ln, const char* fn )
+{
+	lname = ln;
+	fn = "Heyyou";
+	fname[0] = *fn;
+}
+
+void Person::Show()const
+{
+	std::cout << lname;
+}
+
+void Person::FormalShow()const
+{
+	std::cout << fname;
+}
+
+Person::~Person()
+{
+}
