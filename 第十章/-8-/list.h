bc9635f604abe21151c922a9f132b540b8b40489
wpy-learn <140713813+wpy-learn@users.noreply.github.com>
2023-11-10 17:11:38 +0800

    Create list.h

---
+++ 第十章/-8-/list.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef LIST_H
+#define LIST_H
+
+typedef unsigned long Item;
+class List
+{
+public:
+	List();
+	bool isempty() const;//判断是否为空
+	bool isfull() const;//判断是否为满
+	bool insert(Item item);//插入数据
+	void visit(void (*pf)(Item&));
+	~List();
+
+private:
+	enum{ MAX = 50 };
+	Item items[MAX];
+	int top;
+};
+
+#endif // !LIST_H
